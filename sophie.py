4cb3de37f90336ed9c486820991822144a411493
Antonis Farangitakis <afarazit@gmail.com>
2016-07-31 15:11:55 +0300

    Fixing public Www path

--- sophie.py
+++ sophie.py
@@ -168,7 +168,7 @@ class Sophie:
             print("\t%s" % base_path)
             create_dir(base_path)
 
-        vhost_path = _get_path(self.http_www_path) + self.vhost + _get_public_path(
+        vhost_path = _get_path(self.http_www_path) + self.vhost + os.sep + _get_public_path(
             self.http_public_path, self.enable_public_path)
         print("\t%s" % vhost_path)
         create_dir(vhost_path)
