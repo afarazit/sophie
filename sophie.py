c6f364c4e9984ed36cd7c386d68684ad17c0bcdd
Antonis Farangitakis <afarazit@gmail.com>
2016-07-31 11:00:10 +0300

    Changing directories seperator, fixing vhost conf bug, enabling git init

--- sophie.py
+++ sophie.py
@@ -17,20 +17,24 @@ http_public_path = 'public/'
 
 git_repo_base_path = 'c:/Users/afarazit/temp/var_repos/'  # ex. /var/repos/
 git_repo_conf_tpl = 'c:/Users/afarazit/temp/git.tpl'  # ex. /var/repos/post-receive.tpl
-git_checkout_path = 'public_html'  # ex. public_html/, set this to your vhost root folder
+git_checkout_path = 'public_html/'  # ex. public_html/, set this to your vhost root folder
 
-git_executable = '"c:\Program Files\Git\\bin\git.exe" '
+git_executable = 'git.exe '
 
 
 def _get_path(path):
-    return path.rstrip(os.pathsep) + os.altsep
+    return path.rstrip(os.sep).rstrip(os.altsep) + os.sep
 
 
 def _get_public_path(path):
     if path == '':
         return
 
-    return path.rstrip(os.pathsep) + os.altsep
+    return path.rstrip(os.sep).rstrip(os.altsep) + os.sep
+
+
+def _check_for_access():
+    print("TODO: check for file and folder access")
 
 
 class Sophie:
@@ -52,19 +56,22 @@ class Sophie:
 
         print('Hostname is %s' % self.vhost)
 
+        _check_for_access()
         self.read_config()
         if self.vhost_exists():
             exit('Exiting, hostname already exists...')
 
         if enable_vhost_creation:
             replacements = {'{vhost}': self.vhost,
-                            '{vhost_document_root}': _get_path(http_www_path) + _get_public_path(http_public_path)}
+                            '{vhost_document_root}': _get_path(http_www_path) + self.vhost + os.sep + _get_public_path(
+                                http_public_path)}
             self.create_vhost_dir()
             self.create_vhost_conf(replacements)
 
         if enable_git_creation:
-            replacements = {'{vhost_path}': _get_path(http_www_path) + self.repo + os.altsep + _get_path(git_checkout_path),
-                            '{vhost_repo_path}': _get_path(git_repo_base_path) + self.repo}
+            replacements = {
+                '{vhost_path}': _get_path(http_www_path) + self.repo + os.sep + _get_path(git_checkout_path),
+                '{vhost_repo_path}': _get_path(git_repo_base_path) + self.repo}
             self.create_repo_dir()
             self.create_repo_conf(replacements)
 
@@ -86,11 +93,11 @@ class Sophie:
         print("Creating web server directories.")
         base_path = _get_path(http_www_path)
         if not os.path.exists(base_path):
-            print("Creating %s" % base_path)
+            print("\t%s" % base_path)
             os.makedirs(base_path)
 
         vhost_path = _get_path(http_www_path) + self.vhost
-        print("Creating %s" % vhost_path)
+        print("\t%s" % vhost_path)
         os.makedirs(vhost_path)
 
     def create_vhost_conf(self, replacements):
@@ -104,23 +111,23 @@ class Sophie:
         print("Creating git directories.")
         base_path = _get_path(git_repo_base_path)
         if not os.path.exists(base_path):
-            print("Creating %s" % base_path)
+            print("\t%s" % base_path)
             os.makedirs(base_path)
 
         repo_path = _get_path(git_repo_base_path) + self.repo
-        print("Creating %s" % repo_path)
+        print("\t%s" % repo_path)
         os.makedirs(repo_path)
 
-        # TODO this should be created from git init --bare
-        print("Creating %s" % repo_path + os.altsep + 'hooks')
-        os.makedirs(repo_path + os.altsep + 'hooks')
-        # print(git_executable + 'init --bare ' + repo_path)
-        # process = subprocess.Popen([git_executable + ' init --bare ' + repo_path], stdout=subprocess.PIPE)
-        # test = process.communicate()[0]
+        # TODO this should be created via git init --bare
+        # print("\t%s" % repo_path + os.sep + 'hooks')
+        # os.makedirs(repo_path + os.sep + 'hooks')
+        print(git_executable + 'init --bare ' + repo_path)
+        process = subprocess.Popen([git_executable + ' init --bare ' + repo_path], stdout=subprocess.PIPE)
+        test = process.communicate()[0]
 
     def create_repo_conf(self, replacements):
         print("Copying hook post-receive.")
-        hooks_path = _get_path(git_repo_base_path) + self.repo + os.altsep + 'hooks' + os.altsep
+        hooks_path = _get_path(git_repo_base_path) + self.repo + os.sep + 'hooks' + os.sep
         with open(git_repo_conf_tpl) as infile, open(hooks_path + 'post-receive', 'w') as outfile:
             for line in infile:
                 for src, target in replacements.items():
